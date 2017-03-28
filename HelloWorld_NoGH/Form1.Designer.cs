22f0c24dba04508f4f75e812a991b61540df709e
Milton Gonzalez <milton.gonzalez@armeta.com>
2017-03-28 17:14:30 -0500

    New Hello World Text

--- HelloWorld_NoGH/Form1.Designer.cs
+++ HelloWorld_NoGH/Form1.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(31, 98);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(210, 13);
+            this.label1.Size = new System.Drawing.Size(107, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Master with Hello World - Edited from Local";
+            this.label1.Text = "New Hello World text";
             // 
             // Form1
             // 
